fbf5fff0a0a29de83715e3aeaf2576a9f8c53593
TheProgrammer400 <shrey.b.tank@gmail.com>
2026-09-06 22:39:45 +0530

    fix: update GitHub OIDC thumbprint after certificate rotation

--- infrastructure/terraform/iam_github_oidc.tf
+++ infrastructure/terraform/iam_github_oidc.tf
@@ -4,6 +4,7 @@ resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [
+    "227203b5317f3818cab5b5ce596132bf36748c0e",
     "6938fd4d98bab03faadb97b34396831e3780aea1",
     "1c58a2a8515a675928b314324151f8b763f36f46"
   ]
