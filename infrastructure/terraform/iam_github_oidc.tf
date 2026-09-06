4518ad7613610306457e4e000acb5a3fdf11da12
TheProgrammer400 <shrey.b.tank@gmail.com>
2026-09-06 17:08:08 +0530

    allow flexible IAM role configuration and add case-insensitive repository support for OIDC authentication

--- infrastructure/terraform/iam_github_oidc.tf
+++ infrastructure/terraform/iam_github_oidc.tf
@@ -26,7 +26,10 @@ resource "aws_iam_role" "github_cd_deploy" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_repository}:*",
+              "repo:${lower(var.github_repository)}:*"
+            ]
           }
         }
       }
